a06dcea392734b837378ebdb98a42ace3f761048
Mahreen Choudhry <mahreen.ch.3107@gmail.com>
2026-07-15 11:37:14 +0500

    leetcode 3658  solution

---
+++ 3658_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,15 @@
+/*
+3658. GCD of Odd and Even Sums
+
+solution:
+
+****************************************************
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+*/
