93c2789813b207e5187c300762b94b5977c6990c
Mahreen Choudhry <mahreen.ch.3107@gmail.com>
2026-07-25 13:22:08 +0500

    leetcode 3536  solution

---
+++ leetcode_3536_Maximum_Product_of_Two_Digits.cpp
@@ -0,0 +1,20 @@
+/*
+3536. Maximum Product of Two Digits
+
+solution:
+*******************************************
+
+
+
+
+class Solution {
+ public:
+  int maxProduct(int n) {
+    string s = to_string(n);
+    ranges::sort(s);
+    const int sz = s.length();
+    return (s[sz - 1] - '0') * (s[sz - 2] - '0');
+  }
+};
+
+*/
