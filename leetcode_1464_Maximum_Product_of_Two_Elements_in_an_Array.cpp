cec6e1039ddb0d33cf9756f38f5ceaaadcc75792
Mahreen Choudhry <mahreen.ch.3107@gmail.com>
2026-07-27 09:48:56 +0500

    leetcode 1464  solution

---
+++ leetcode_1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp
@@ -0,0 +1,21 @@
+/*
+1464. Maximum Product of Two Elements in an Array
+
+solution:
+*********************
+class Solution {
+ public:
+  int maxProduct(vector<int>& nums) {
+    int max1 = 0;
+    int max2 = 0;
+
+    for (const int num : nums)
+      if (num > max1)
+        max2 = std::exchange(max1, num);
+      else if (num > max2)
+        max2 = num;
+
+    return (max1 - 1) * (max2 - 1);
+  }
+};
+*/
